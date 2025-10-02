06de3f65f62ecac6e93be776e069c34d428c42dc
Arjan Molenaar <gaphor@gmail.com>
2025-10-02 17:56:52 +0200

    Move extension modules to toplevel
    Having the (C-) extension modules on toplevel allows
    modules to be initialized with `PyImport_AppendInittab`, which would
    potentially make PyGObject usable on Android.

--- tests/conftest.py
+++ tests/conftest.py
@@ -79,9 +79,9 @@ def pytest_configure(config):
     sys.path.insert(0, srcdir)
 
     import gi
+    import _gi
 
-    gi_builddir = os.path.dirname(gi._gi.__file__)
-    builddir = os.path.dirname(gi_builddir)
+    builddir = os.path.dirname(_gi.__file__)
     tests_builddir = os.path.join(builddir, "tests")
 
     sys.path.insert(0, tests_builddir)
